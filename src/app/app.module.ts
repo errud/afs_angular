d23c2992ab9ae31bb9eb998b4c68cee412c4e426
errud <ericrudz@gmail.com>
2022-06-14 13:53:20 +0200

    first depot angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+import { getInstructionStatements } from '@angular/compiler/src/render3/view/util';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AuthComponent } from './components/Access/auth/auth.component';
+import { LoginComponent } from './components/Access/login/login.component';
+import { RegisterComponent } from './components/Access/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminMenuComponent } from './components/nav/admin-menu/admin-menu.component';
+import { ProfileComponent } from './components/User/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AdminMenuComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
